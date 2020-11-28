82ae08f0050579fd7b343b62a9218b1c4e4e8508
Erick Martim <erick.martim@i9xp.com.br>
2020-11-28 20:42:16 -0300

    Adiçao da tela de mapa e navegaçao do app

---
+++ src/pages/OrphanageDetails.tsx
@@ -0,0 +1,7 @@
+import { DefaultTransition } from '@react-navigation/stack/lib/typescript/src/TransitionConfigs/TransitionPresets';
+import React from 'react';
+import { View } from 'react-native';
+
+export default function OrphanageDetails() {
+  return <View />
+}
